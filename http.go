5d24d4074f1a4457285215b01ec3fb76bcce9126
Sam Ban <sam@gpmd.co.uk>
2018-11-26 15:05:55 +0000

    add content-type attribute

--- http.go
+++ http.go
@@ -20,15 +20,44 @@ type HTTPAuth struct {
 	Password string
 }
 
+// HTTPRequestWithContentType is a helper function creating a http requests
+func HTTPRequestWithContentType(method, url string, requestBody io.Reader, auth HTTPAuth, contentType string) ([]byte, error) {
+	body, _, err := HTTPRequestResp(method, url, requestBody, auth, contentType)
+	return body, err
+}
+
+// ParsedHTTPRequestWithContentType is a helper function creating a http requests parsing JSON or XML response
+func ParsedHTTPRequestWithContentType(method, url string, requestBody io.Reader, auth HTTPAuth, contentType string) ([]byte, interface{}, error) {
+	body, resp, err := HTTPRequestResp(method, url, requestBody, auth, contentType)
+	if err != nil {
+		slog.Infof("Error reading from URL %s %v", url, err)
+		return body, resp, err
+	}
+	var parsedbody interface{}
+	if strings.HasPrefix(resp.Header.Get("Content-Type"), "application/json") {
+		err = json.Unmarshal(body, &parsedbody)
+		if err != nil {
+			slog.Infof("Error parsing body JSON %s %v", url, err)
+		}
+	} else if strings.HasPrefix(resp.Header.Get("Content-Type"), "application/xml") {
+		m, err := mxj.NewMapXml(body)
+		if err != nil {
+			slog.Infof("Error parsing body XML %s %v", url, err)
+		}
+		return body, m, err
+	}
+	return body, parsedbody, err
+}
+
 // HTTPRequest is a helper function creating a http requests
 func HTTPRequest(method, url string, requestBody io.Reader, auth HTTPAuth) ([]byte, error) {
-	body, _, err := HTTPRequestResp(method, url, requestBody, auth)
+	body, _, err := HTTPRequestResp(method, url, requestBody, auth, "application/json")
 	return body, err
 }
 
 // ParsedHTTPRequest is a helper function creating a http requests parsing JSON or XML response
 func ParsedHTTPRequest(method, url string, requestBody io.Reader, auth HTTPAuth) ([]byte, interface{}, error) {
-	body, resp, err := HTTPRequestResp(method, url, requestBody, auth)
+	body, resp, err := HTTPRequestResp(method, url, requestBody, auth, "application/json")
 	if err != nil {
 		slog.Infof("Error reading from URL %s %v", url, err)
 		return body, resp, err
@@ -50,13 +79,13 @@ func ParsedHTTPRequest(method, url string, requestBody io.Reader, auth HTTPAuth)
 }
 
 // HTTPRequestResp is a helper function creating a http requests
-func HTTPRequestResp(method, url string, requestBody io.Reader, auth HTTPAuth) ([]byte, *http.Response, error) {
+func HTTPRequestResp(method, url string, requestBody io.Reader, auth HTTPAuth, contentType string) ([]byte, *http.Response, error) {
 	req, err := http.NewRequest(method, url, requestBody)
 	if err != nil {
 		slog.Infof("Error creating request: %v", err)
 		return nil, &http.Response{}, fmt.Errorf("Error creating request: %v", err)
 	}
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", contentType)
 	if auth.User != "" {
 		req.SetBasicAuth(auth.User, auth.Password)
 	}
