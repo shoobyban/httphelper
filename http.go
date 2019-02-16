f531846449c6210603e1d307453973cf7ea6bc56
Sam Ban <sam@gpmd.co.uk>
2019-02-16 09:49:26 +0000

    less log, more concise

--- http.go
+++ http.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/shoobyban/mxj"
-	"github.com/shoobyban/slog"
 )
 
 // HTTPAuth is used for HTTPRequest
@@ -27,63 +26,44 @@ func HTTPRequestWithHeaders(method, url string, requestBody io.Reader, auth HTTP
 	return body, err
 }
 
-// ParsedHTTPRequestWithHeaders is a helper function creating a http requests parsing JSON or XML response
-func ParsedHTTPRequestWithHeaders(method, url string, requestBody io.Reader, auth HTTPAuth, headers map[string]string) ([]byte, interface{}, error) {
-	body, resp, err := HTTPRequestResp(method, url, requestBody, auth, headers)
-	if err != nil {
-		slog.Infof("Error reading from URL %s %v", url, err)
-		return body, resp, err
-	}
-	var parsedbody interface{}
-	if strings.HasPrefix(resp.Header.Get("Content-Type"), "application/json") {
-		err = json.Unmarshal(body, &parsedbody)
-		if err != nil {
-			slog.Infof("Error parsing body JSON %s %v", url, err)
-		}
-	} else if strings.Contains(resp.Header.Get("Content-Type"), "xml") {
-		m, err := mxj.NewMapXml(body)
-		if err != nil {
-			slog.Infof("Error parsing body XML %s %v", url, err)
-		}
-		return body, m, err
-	}
-	return body, parsedbody, err
-}
-
 // HTTPRequest is a helper function creating a http requests
 func HTTPRequest(method, url string, requestBody io.Reader, auth HTTPAuth) ([]byte, error) {
 	body, _, err := HTTPRequestResp(method, url, requestBody, auth, map[string]string{})
 	return body, err
 }
 
-// ParsedHTTPRequest is a helper function creating a http requests parsing JSON or XML response
-func ParsedHTTPRequest(method, url string, requestBody io.Reader, auth HTTPAuth) ([]byte, interface{}, error) {
-	body, resp, err := HTTPRequestResp(method, url, requestBody, auth, map[string]string{})
+// ParsedHTTPRequestWithHeaders is a helper function creating a http requests parsing JSON or XML response
+func ParsedHTTPRequestWithHeaders(method, url string, requestBody io.Reader, auth HTTPAuth, headers map[string]string) ([]byte, interface{}, error) {
+	body, resp, err := HTTPRequestResp(method, url, requestBody, auth, headers)
 	if err != nil {
-		slog.Infof("Error reading from URL %s %v", url, err)
-		return body, resp, err
+		return body, resp, fmt.Errorf("Error reading from URL %s %v", url, err)
 	}
 	var parsedbody interface{}
 	if strings.HasPrefix(resp.Header.Get("Content-Type"), "application/json") {
 		err = json.Unmarshal(body, &parsedbody)
 		if err != nil {
-			slog.Infof("Error parsing body JSON %s %v", url, err)
+			return body, parsedbody, fmt.Errorf("Error parsing body JSON %s %v", url, err)
 		}
 	} else if strings.Contains(resp.Header.Get("Content-Type"), "xml") {
 		m, err := mxj.NewMapXml(body)
 		if err != nil {
-			slog.Infof("Error parsing body XML %s %v", url, err)
+			return body, m, fmt.Errorf("Error parsing body XML %s %v", url, err)
 		}
-		return body, m, err
+		return body, m, nil
 	}
 	return body, parsedbody, err
+}
+
+// ParsedHTTPRequest is a helper function creating a http requests parsing JSON or XML response
+func ParsedHTTPRequest(method, url string, requestBody io.Reader, auth HTTPAuth) ([]byte, interface{}, error) {
+	return ParsedHTTPRequestWithHeaders(method, url, requestBody, auth, map[string]string{})
 }
 
 // HTTPRequestResp is a helper function creating a http requests
 func HTTPRequestResp(method, url string, requestBody io.Reader, auth HTTPAuth, headers map[string]string) ([]byte, *http.Response, error) {
 	req, err := http.NewRequest(method, url, requestBody)
 	if err != nil {
-		slog.Infof("Error creating request: %v", err)
+		//		slog.Infof("Error creating request: %v", err)
 		return nil, &http.Response{}, fmt.Errorf("Error creating request: %v", err)
 	}
 	req.Header.Set("Content-Type", "application/json")
@@ -107,22 +87,18 @@ func HTTPRequestResp(method, url string, requestBody io.Reader, auth HTTPAuth, h
 
 	resp, err := client.Do(req)
 	if err != nil {
-		slog.Infof("Error connecting to URL %s: %v", url, err)
 		return nil, resp, fmt.Errorf("Error connecting to URL %s: %v", url, err)
 	}
 	defer resp.Body.Close()
 
-	slog.Infof("response Status: %v", resp.Status)
-	slog.Infof("response Headers: %v", resp.Header)
+	//	slog.Infof("response Status: %v", resp.Status)
+	//	slog.Infof("response Headers: %v", resp.Header)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		slog.Infof("Error reading from URL %s: %v", url, err)
-		return body, resp, fmt.Errorf("Error while connecting to URL")
+		return body, resp, fmt.Errorf("Error reading from URL %s: %v", url, err)
 	}
 	if resp.StatusCode > 399 {
-		slog.Infof("Error reading from URL %s, status: %d, body: %s error:%v", url, resp.StatusCode, string(body), err)
 		return body, resp, fmt.Errorf("Error reading from URL %s, status: %d, body: %s error:%v", url, resp.StatusCode, string(body), err)
 	}
-	//	slog.Infof("response Body: %s", string(body))
 	return body, resp, nil
 }
